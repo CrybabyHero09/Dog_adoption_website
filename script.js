69005b082892aa054475a1414fef8b32a8989f1e
CrybabyHero <suyashsakore2151@gmail.com>
2025-08-15 22:25:26 +0530

    Initial commit - Dog Adoption Website

---
+++ script.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function () {
+      const hamburger = document.getElementById("hamburger");
+      const dropdown = document.getElementById("dropdown");
+
+      hamburger.addEventListener("click", function () {
+        dropdown.classList.toggle("show");
+      });
+    });
+
+function goToLogin() {
+  window.location.href = "login.html";
+}
+
+function goToSignup() {
+  window.location.href = "signup.html";
+}
